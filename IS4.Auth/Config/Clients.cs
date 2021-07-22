7559e2972253964714d8bbfba195c219a09c99b9
Dominik Wikliński <domel2222@gmail.com>
2021-07-22 11:00:48 +0200

    Add project files.

---
+++ IS4.Auth/Config/Clients.cs
@@ -0,0 +1,29 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS4.Auth.Config
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "FirstClient",
+                    ClientName = "Sample Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List <Secret> { new Secret("client_secret".ToSha256())},
+                    AllowedScopes = new List<string> {"invoices.write invoices.read"}
+                }
+            };
+        }
+    }
+}
---
+++ IS4.Auth/Config/Resources.cs
@@ -0,0 +1,44 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS4.Auth.Config
+{
+    public class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> { "role"}
+                }
+            };
+        }
+
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource
+                {
+                    Name = "invoices",
+                    DisplayName = "Invoices",
+                    Description = "Invoices",
+                    Scopes = new List<string> {"invoices.read", "invoices.write"},
+                    ApiSecrets = new List<Secret> { new Secret("client_secret".ToSha256())}
+                }
+            };
+        }
+    }
+}
